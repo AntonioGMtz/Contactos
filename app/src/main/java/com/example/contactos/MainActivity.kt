511a7a738f8190c7bb59f46812235f26740d20ca
Antonio <antoniogallegos086@gmail.com>
2022-05-05 21:30:17 -0500

    Room + Adapter 1-version

--- app/src/main/java/com/example/contactos/MainActivity.kt
+++ app/src/main/java/com/example/contactos/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.contactos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import com.example.contactos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding : ActivityMainBinding
+
+    companion object{
+         var editMode = true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        editMode = true
+
+        mBinding.fabAdd.setOnClickListener{
+            launchEditFragment()
+            mBinding.lilContenido.visibility = View.GONE
+        }
+
+    }
+
+
+    private fun launchEditFragment(args : Bundle? = null) { //SINTAXIS BASICA PARA INICIAR UN FRAGMENT
+
+        val fragment = Fragment_add_datas() //Instanciamos el Fragment
+        if(args != null) fragment.arguments = args
+        val fragmentManager = supportFragmentManager //Gestor para controlar los fragmentos
+        val fragmentTransaction = fragmentManager.beginTransaction() //Dcide como ejecutar el fragment
+        fragmentTransaction.add(R.id.containerMain,fragment) //LLAMA AL AFRAGMENT
+        fragmentTransaction.commit()//Inicia el fragment
+        fragmentTransaction.addToBackStack(null)
+        //Si es true muestra el boton flotante si no lo esconde
+
     }
 }
